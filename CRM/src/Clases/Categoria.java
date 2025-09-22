e299371a7732db0d9bbde69e09e7551a4841ae6e
kvicentev3-collab <kvicentev3@miumg.edu.gt>
2025-09-22 16:04:57 -0600

    Add files via upload

---
+++ CRM/src/Clases/Categoria.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author kenet
+ */
+public class Categoria {
+ private int id;
+    private String nombre;
+   
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public boolean  Guardar(){
+    
+      Connection _conexion = null;
+    try {
+        String conexionString ="jdbc:mysql://localhost/crm?characterEncoding=latin1";
+        String driverName ="com.mysql.cj.jdbc.Driver";  //com.mysql.jdbc.Driver;
+        Class.forName(driverName).newInstance();
+       _conexion = DriverManager.getConnection(conexionString, "root","012003"); 
+       _conexion.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+      
+       Statement st = _conexion.createStatement();
+       st.execute("insert into categoriaproductos (nombre)values ('"+getNombre()+"')");
+       
+      // System.out.println("Conexion exitosa!!!");
+    
+      return true;
+       
+    }catch (Exception ex){ 
+        System.out.println("Error" + ex.getMessage());
+     return false;
+    }finally {
+        try {
+        _conexion.close();
+   }catch (Exception ex2){   
+   }        
+}
+    
+}
+}
---
+++ CRM/src/Clases/ComboBox.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author kenet
+ */
+public class ComboBox {
+
+    private int Id;
+   private String Nombre;
+   
+   public ComboBox( int Id, String Nombre){
+       this.Id=Id;
+       this.Nombre=Nombre;
+       
+   }
+   
+    @Override
+    public String toString(){
+        return Nombre; 
+    }
+   /**
+     * @return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+ 
+}
